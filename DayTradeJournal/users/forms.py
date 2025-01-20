12baa0506df07172e286d6cb9102eecb9f421f77
Jordan Stewart <121924540+jstewart4u2c@users.noreply.github.com>
2025-01-20 15:48:19 -0700

    Styling and Profile Page

--- DayTradeJournal/users/forms.py
+++ DayTradeJournal/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -10,5 +10,3 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["first_name", "username", "email", "password1", "password2"]
-
-
